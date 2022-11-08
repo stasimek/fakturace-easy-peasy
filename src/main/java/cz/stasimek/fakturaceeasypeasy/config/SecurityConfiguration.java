8c223251367633ff440656057c0b2308edb33a9b
Stanislav Simek <stasimek@gmail.com>
2022-11-09 00:01:26 +0100

    Logout button

--- src/main/java/cz/stasimek/fakturaceeasypeasy/config/SecurityConfiguration.java
+++ src/main/java/cz/stasimek/fakturaceeasypeasy/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfiguration {
@@ -13,12 +14,22 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+				// URLs available without login.
 				.authorizeRequests(
 						a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
 								.anyRequest().authenticated()
 				)
 				.exceptionHandling(
 						e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				// Spring Security has built in support for a /logout endpoint
+				// which clear the session and invalidate the cookie.
+				.logout(
+						l -> l.logoutSuccessUrl("/").permitAll()
+				)
+				// Create cookie XSRF-TOKEN.
+				.csrf(
+						c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				)
 				.oauth2Login();
 		return http.build();
