263e91525a150899b8b3ba64702ebc91d6c81276
Stanislav Simek <stasimek@gmail.com>
2022-11-08 13:49:22 +0100

    SpringBoot bootstrap generated at https://start.spring.io

---
+++ src/main/java/cz/stasimek/fakturaceeasypeasy/FakturaceEasyPeasyApplication.java
@@ -0,0 +1,13 @@
+package cz.stasimek.fakturaceeasypeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturaceEasyPeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturaceEasyPeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/stasimek/fakturaceeasypeasy/FakturaceEasyPeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.stasimek.fakturaceeasypeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturaceEasyPeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
