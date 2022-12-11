ec65a059a1fa60f085226f0ff950425b61545aae
Stanislav Simek <stasimek@gmail.com>
2022-12-11 18:41:47 +0100

    Adding support for listOfObjects to Options component.

--- frontend-react-js/src/component/html/Options.js
+++ frontend-react-js/src/component/html/Options.js
@@ -1,21 +1,44 @@
 import React, { Component, Fragment } from 'react';
 import { withTranslation } from 'react-i18next';
 
+/**
+ * Render <option>s using parameter "map" or "list" as a source.
+ * 
+ * Usage:
+ *     <Options map={object}/>
+ *     or
+ *     <Options list={listOfObjects} key="keyAttribute" value="valueAttribute"/>
+ *
+ * Optional attributes:
+ *     nonEmpty - Don't render empty option.
+ */
 class Options extends Component {
 
 	render() {
-		if (!this.props.map) {
+		if (!this.props.map && !(this.props.list && this.props.key && this.props.value)) {
 			return;
 		}
 
 		const {t} = this.props;
 		const emptyOption = this.props.nonEmpty === undefined ? <option>{t('--')}</option> : '';
-		return (
-			<Fragment>
-				{emptyOption}
-				{Object.entries(this.props.map).map(v => <option value={v[0]}>{v[1]}</option>)}
-			</Fragment>
-		);
+		if (this.props.map) {
+			return (
+				<Fragment>
+					{emptyOption}
+					{Object.entries(this.props.map).map(v => <option value={v[0]}>{v[1]}</option>)}
+				</Fragment>
+			)
+		} else {
+			return (
+				<Fragment>
+					{emptyOption}
+					{this.props.list.map(
+							v => <option value={v[this.props.key]}>{v[this.props.value]}</option>
+					)}
+				</Fragment>
+			)
+		}
+
 	}
 }
 
