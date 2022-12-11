bfa8bad3ac10a2e21c812fff17194616ab723365
Stanislav Simek <stasimek@gmail.com>
2022-12-11 18:34:39 +0100

    Adding support for sm and md attributes to FormRow component.

--- frontend-react-js/src/component/html/FormRow.js
+++ frontend-react-js/src/component/html/FormRow.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
 
+/**
+ * Will render <Row> with <Col>s for all children elements.
+ *
+ * Usage:
+ *     <FormRow>
+ *         <FormGroup md="3" sm="6">
+ *         </FormGroup>
+ *         <FormGroup md="9" sm="6">
+ *         </FormGroup>
+ *     </FormRow>
+ *
+ * Defaults:
+ *     - md="3"
+ *     - sm="6"
+ */
 export default class FormRow extends Component {
 
 	render() {
 		return (
-			<Row>
-				{React.Children.map(this.props.children, child => <Col md="3" sm="6">{child}</Col>)}
-			</Row>
+				<Row {...this.props}>
+					{React.Children.map(
+							this.props.children,
+							child => {
+								const md = child.props.md || 3;
+								const sm = child.props.sm || 6;
+								return <Col md={md} sm={sm}>{child}</Col>;
+							}
+					)}
+				</Row>
 		);
 	}
 }
